c4121fa085f3936d1cfe35c53a9948dccd632556
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-09-07 10:47:00 +0200

    fixed addon descriptions

--- G1ANT.Addon.LotusNotes/LotusNotesAddon.cs
+++ G1ANT.Addon.LotusNotes/LotusNotesAddon.cs
@@ -14,7 +14,7 @@ namespace G1ANT.Addon.LotusNotes
     [Addon(Name = "LotusNotes", Tooltip = "Addon for Lotus Notes automations")]
     [Copyright(Author = "G1ANT Robot Ltd", Copyright = "G1ANT Robot Ltd", Email = "hi@g1ant.com", Website = "www.g1ant.com")]
     [License(Type = "LGPL", ResourceName = "License.txt")]
-    [CommandGroup(Name = "sap", Tooltip = "Command allows Lotus Notes automation")]
+    [CommandGroup(Name = "notes", Tooltip = "Commands that allow Lotus Notes automation")]
     public class SAPAddon : Language.Addon
     {
     }
